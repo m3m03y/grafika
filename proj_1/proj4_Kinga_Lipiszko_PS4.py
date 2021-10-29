f4f4419480662c1bd6b9111da1030153934e75b0
Kinga L <memoey99@gmail.com>
2021-10-29 21:47:47 +0200

    przekształcenia punktowe

--- proj_1/proj4_Kinga_Lipiszko_PS4.py
+++ proj_1/proj4_Kinga_Lipiszko_PS4.py
@@ -11,39 +11,82 @@ import re
 import webbrowser
 
 MODES = [
-    "Addition",             #0
-    "Subtraction",          #1
-    "Multiplication",       #2
-    "Division",             #3
-    "Brightness",           #4
-    "To greyscale"          #5
+    "Addition",                             #0
+    "Subtraction",                          #1
+    "Multiplication",                       #2
+    "Division",                             #3
+    "Adjust brightness",                    #4
+    "Convert to greyscale (ratio)",         #5
+    "Convert to greyscale (average)"        #6
     ]
+
 MIN_VAL = 0
 MAX_VAL = 255
 MAX_COLOR_VALUE = 255
+
 class ImageConverter:
     def __processImage(self, func, image, val):
         for y in range (image.height()):
             for x in range (image.width()):
                 pix = image.pixel(x,y)
                 r,g,b = qRed(pix), qGreen(pix), qBlue(pix)
-                r = func(r, val)
-                g = func(g, val)
-                b = func(b, val)
+                r, g, b = func([r, g, b], val)
                 image.setPixelColor(x,y, QColor(r,g,b))
         return image
 
-    def __add(self, current, val):
-        return min((current + val), 255)
+    def __add(self, current, val): 
+        for i in range(len(current)):
+            current[i] = min((current[i] + val), 255)
+        return current
 
     def __subtract(self, current, val):
-        return max((current - val), 0)
+        for i in range(len(current)):
+            current[i] = max((current[i] - val), 0)
+        return current
 
     def __multiply(self, current, val):
-        return min((current * val), 255)
+        for i in range(len(current)):
+            current[i] = min((current[i] * val), 255)
+        return current
 
     def __divide(self, current, val):
-        return max((round(current % val)), 0)
+        for i in range(len(current)):
+            current[i] = max((round(current[i] / val)), 0)
+        return current
+
+    def __findMiddle(self, arr):
+        idx = -1
+        maxValue = max(arr)
+        minValue = min(arr)
+        for i in range(len(arr)):
+            if (maxValue != arr[i]) and (minValue != arr[i]):
+                idx = i
+        if idx == -1: idx = 0
+        return idx
+
+    def __roundColor(self, val):
+        if (val > 255): return 255
+        elif (val < 0): return 0
+        return val
+
+    def __calculateBrightness(self, current, val):
+        if (val < 0):
+            scale = (1.0 - (abs(val) / 100))
+            for i in range(len(current)):
+                current[i] = self.__roundColor(round(current[i] * scale))
+        elif (val > 0):
+            for i in range(len(current)):
+                difference = MAX_VAL - current[i]
+                current[i] = self.__roundColor(round(difference * (val / 100) + current[i]))
+        return current
+
+    def __convertToGrayscaleRatio(self, current, val):
+        Y = round(0.299 * current[0] + 0.587 * current[1] + 0.114 * current[2])
+        return [Y,Y,Y]    
+    
+    def __convertToGrayscaleAverage(self, current, val):
+        avg = round((current[0] + current[1] + current[2]) / 3)
+        return [avg,avg,avg]
 
     def add(self, image, value):
         image = self.__processImage(self.__add, image, value)
@@ -61,6 +104,18 @@ class ImageConverter:
         image = self.__processImage(self.__divide, image, value)
         return image
 
+    def changeBrightness(self, image, value):
+        image = self.__processImage(self.__calculateBrightness, image, value)
+        return image
+    
+    def convertToGrayscaleRatio(self, image, value):
+        image = self.__processImage(self.__convertToGrayscaleRatio, image, value)
+        return image    
+    
+    def convertToGrayscaleAverage(self, image, value):
+        image = self.__processImage(self.__convertToGrayscaleAverage, image, value)
+        return image
+
 class Form(QDialog):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
@@ -74,6 +129,7 @@ class Form(QDialog):
         self.layout.addRow(self.image_label)
         self.menu = QComboBox()
         self.menu.addItems(MODES)
+        self.menu.currentIndexChanged.connect(self.__changeMode)
         self.layout.addRow(self.menu)
         self.layout.addRow(self.__createInput())
 
@@ -92,7 +148,6 @@ class Form(QDialog):
         self.img = QImage(filePath)
         self.__fixScale()
         self.__showImage()
-
 
     def __showImage(self):
         self.image_label.setPixmap(QPixmap.fromImage(self.img))
@@ -127,9 +182,37 @@ class Form(QDialog):
             if (int(val) == 0):
                 self.__showErrorMessage("Cannot divide by zero", "Invalid value")
                 return
-            self.img = self.converter.divide(self.img,val)
+            self.img = self.converter.divide(self.img,val)        
+        elif (int(pos) == 4):
+            self.img = self.converter.changeBrightness(self.img,val)        
+        elif (int(pos) == 5):
+            self.img = self.converter.convertToGrayscaleRatio(self.img,val)        
+        elif (int(pos) == 6):
+            self.img = self.converter.convertToGrayscaleAverage(self.img,val)
         self.__fixScale()
         self.__showImage()
+
+    def __changeMode(self):
+        pos = self.menu.currentIndex()
+        if (pos >= 0) and (pos <= 3):
+            self.__setMinMaxValues(MIN_VAL,MAX_VAL)
+        elif (pos == 4):
+            self.__setMinMaxValues(-75,75)
+        elif (pos == 5) or (pos == 6):
+            self.__setMinMaxValues(0,0)
+
+
+    def __setMinMaxValues(self, minVal, maxVal):
+        self.__setSliderMinMaxValues(minVal, maxVal)
+        self.__setSpinBoxMinMaxValues(minVal, maxVal)
+
+    def __setSliderMinMaxValues(self, minVal, maxVal):
+        self.slider.setMaximum(maxVal)
+        self.slider.setMinimum(minVal)    
+    
+    def __setSpinBoxMinMaxValues(self, minVal, maxVal):
+        self.spinBox.setMaximum(maxVal)
+        self.spinBox.setMinimum(minVal)
 
     def __sliderAction(self):
         val = self.slider.value()
@@ -147,17 +230,14 @@ class Form(QDialog):
             buttons=QMessageBox.Ignore,
             defaultButton=QMessageBox.Ignore,
         )
-        
+    
     def __createInput(self):
         layout = QHBoxLayout()
         self.slider = QSlider(Qt.Horizontal)
         self.spinBox = QSpinBox()
-        self.slider.setMaximum(MAX_VAL)
-        self.slider.setMinimum(MIN_VAL)
         self.slider.valueChanged.connect(self.__sliderAction)
-        self.spinBox.setMaximum(MAX_VAL)
-        self.spinBox.setMinimum(MIN_VAL)
         self.spinBox.valueChanged.connect(self.__spinBoxAction)
+        self.__setMinMaxValues(MIN_VAL,MAX_VAL)
         layout.addWidget(self.slider)
         layout.addWidget(self.spinBox)
         return layout
