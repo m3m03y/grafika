fc4e84a4bb7a0d57a72eeb8e9605180e63f21715
Kinga L <memoey99@gmail.com>
2021-10-29 20:00:09 +0200

    basic implementation

--- proj_1/proj4_Kinga_Lipiszko_PS4.py
+++ proj_1/proj4_Kinga_Lipiszko_PS4.py
@@ -9,25 +9,80 @@ import pathlib
 import numpy as np
 import re
 import webbrowser
+
+MODES = [
+    "Addition",             #0
+    "Subtraction",          #1
+    "Multiplication",       #2
+    "Division",             #3
+    "Brightness",           #4
+    "To greyscale"          #5
+    ]
+MIN_VAL = 0
+MAX_VAL = 255
+MAX_COLOR_VALUE = 255
+class ImageConverter:
+    def __processImage(self, func, image, val):
+        for y in range (image.height()):
+            for x in range (image.width()):
+                pix = image.pixel(x,y)
+                r,g,b = qRed(pix), qGreen(pix), qBlue(pix)
+                r = func(r, val)
+                g = func(g, val)
+                b = func(b, val)
+                image.setPixelColor(x,y, QColor(r,g,b))
+        return image
+
+    def __add(self, current, val):
+        return min((current + val), 255)
+
+    def __subtract(self, current, val):
+        return max((current - val), 0)
+
+    def __multiply(self, current, val):
+        return min((current * val), 255)
+
+    def __divide(self, current, val):
+        return max((round(current % val)), 0)
+
+    def add(self, image, value):
+        image = self.__processImage(self.__add, image, value)
+        return image
+
+    def subtract(self, image, value):
+        image = self.__processImage(self.__subtract, image, value)
+        return image
+
+    def multiply(self, image, value):
+        image = self.__processImage(self.__multiply, image, value)
+        return image
+
+    def divide(self, image, value):
+        image = self.__processImage(self.__divide, image, value)
+        return image
+
 class Form(QDialog):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
-        self.sliderArr = []
-        self.spinBoxArr = []
         openBtn = QPushButton("Open file")
-        openBtn.clicked.connect(self.open)
-
+        openBtn.clicked.connect(self.__open)
+        self.converter = ImageConverter()
+        self.img = None
         self.layout = QFormLayout()
         self.layout.addRow(openBtn)
         self.image_label = QLabel(" ")
         self.layout.addRow(self.image_label)
-        self.layout.addRow(self.__createInput("Add",0,255,self.__customClick(0)))
-        self.layout.addRow(self.__createInput("Sub",0,255,self.__customClick(1)))
-        self.layout.addRow(self.__createInput("Mult",0,255,self.__customClick(2)))
-        self.layout.addRow(self.__createInput("Div",0,255,self.__customClick(3)))
+        self.menu = QComboBox()
+        self.menu.addItems(MODES)
+        self.layout.addRow(self.menu)
+        self.layout.addRow(self.__createInput())
+
+        submitBtn = QPushButton("Submit")
+        submitBtn.clicked.connect(self.__processImage)
+        self.layout.addRow(submitBtn)
         self.setLayout(self.layout)
 
-    def open(self):
+    def __open(self):
         filePath, _ = QFileDialog.getOpenFileName(self, 'Open File', "",
             "Images (*.ppm *.jpeg *.jpg)")
  
@@ -35,33 +90,76 @@ class Form(QDialog):
             return False
 
         self.img = QImage(filePath)
-        if (self.img.height() > 720):
-            self.img = self.img.scaledToHeight(720)
-        if (self.img.width() > 1280):
-            self.img = self.img.scaledToWidth(1280)
+        self.__fixScale()
+        self.__showImage()
+
+
+    def __showImage(self):
         self.image_label.setPixmap(QPixmap.fromImage(self.img))
         self.update()
 
-    def __customClick(self, pos):
-        val = self.sliderArr[pos].value()
-        self.spinBoxArr[pos].setValue(val)
+    def __fixScale(self):
+        if (self.img.height() > 720):
+            self.img = self.img.scaledToHeight(720)
+        elif (self.img.height() < 50):
+            self.img = self.img.scaledToHeight(50)
+        if (self.img.width() > 1280):
+            self.img = self.img.scaledToWidth(1280)        
+        elif (self.img.width() < 50):
+            self.img = self.img.scaledToWidth(50)
 
-    def __createInput(self, name, minVal, maxVal, action):
+    def __processImage(self):
+        if (self.img == None):
+            self.__showErrorMessage("No image selected", "Invalid image")
+            return
+        pos = self.menu.currentIndex()
+        val = self.slider.value()
+
+        print('Mode: {}, value: {}'.format(pos,val))
+
+        if (int(pos) == 0):
+            self.img = self.converter.add(self.img,val)
+        elif (int(pos) == 1):
+            self.img = self.converter.subtract(self.img,val)
+        elif (int(pos) == 2):
+            self.img = self.converter.multiply(self.img,val)
+        elif (int(pos) == 3):
+            if (int(val) == 0):
+                self.__showErrorMessage("Cannot divide by zero", "Invalid value")
+                return
+            self.img = self.converter.divide(self.img,val)
+        self.__fixScale()
+        self.__showImage()
+
+    def __sliderAction(self):
+        val = self.slider.value()
+        self.spinBox.setValue(val)
+
+    def __spinBoxAction(self):
+        val = self.spinBox.value()
+        self.slider.setValue(val)
+
+    def __showErrorMessage(self, msg, title):
+        QMessageBox.critical(
+            self,
+            title,
+            msg,
+            buttons=QMessageBox.Ignore,
+            defaultButton=QMessageBox.Ignore,
+        )
+        
+    def __createInput(self):
         layout = QHBoxLayout()
-        label = QLabel(name)
-        slider = QSlider(Qt.Horizontal)
-        spinBox = QSpinBox()
-        slider.setMaximum(maxVal)
-        slider.setMinimum(minVal)
-        slider.valueChanged.connect(action)
-        spinBox.setMaximum(maxVal)
-        spinBox.setMinimum(minVal)
-        spinBox.valueChanged.connect(action)
-        self.sliderArr.append(slider)
-        self.spinBoxArr.append(spinBox)
-        layout.addWidget(label)
-        layout.addWidget(slider)
-        layout.addWidget(spinBox)
+        self.slider = QSlider(Qt.Horizontal)
+        self.spinBox = QSpinBox()
+        self.slider.setMaximum(MAX_VAL)
+        self.slider.setMinimum(MIN_VAL)
+        self.slider.valueChanged.connect(self.__sliderAction)
+        self.spinBox.setMaximum(MAX_VAL)
+        self.spinBox.setMinimum(MIN_VAL)
+        self.spinBox.valueChanged.connect(self.__spinBoxAction)
+        layout.addWidget(self.slider)
+        layout.addWidget(self.spinBox)
         return layout
 
 if __name__=='__main__':
